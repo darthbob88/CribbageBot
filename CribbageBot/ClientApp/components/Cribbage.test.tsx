8ff77988f42d522cece3535e83b1cbcfc6ef485d
Raymond Robert Price <darthbob88@gmail.com>
2018-09-30 14:26:39 -0700

    Gesture towards adding tests.

---
+++ CribbageBot/ClientApp/components/Cribbage.test.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Cribbage from './Cribbage';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Cribbage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
